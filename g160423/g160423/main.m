5af823eae69958b8e429b45eb380e1f1beff32bf
qhfz <qhfz@CircuitCoder.local>
2016-11-15 19:20:17 +0800

    Initialize

---
+++ g160423/g160423/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  g160423
+//
+//  Created by qhfz on 2016/11/15.
+//  Copyright © 2016年 qhfz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
